c5a5c92f12b0c992d75dd7949b7eb7f197794017
ravishukla86044 <ravishukls86044@gmail.com>
2021-07-21 16:55:02 +0530

    pulling footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import Login from './Component/Login/Login';
-import './App.css';
+import Login from "./Component/Login/Login";
+import "./App.css";
 
 function App() {
-  return (
-    <div className='App'>
-      <Login />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
