902c3b434c0e84575635bb89e519d441615c139a
ravishukla86044 <ravishukls86044@gmail.com>
2021-07-23 17:49:10 +0530

    dash

--- src/Component/Dashboard/LiveEdit.jsx
+++ src/Component/Dashboard/LiveEdit.jsx
@@ -1,6 +1,7 @@
 import styles from "./LiveEdit.module.css";
 import styled from "styled-components";
 import { Nav } from "../Home/Nav";
+import Footer from "../Home/Footer";
 import { useEffect, useState } from "react";
 
 function LiveEdit({ data }) {
@@ -17,16 +18,39 @@ function LiveEdit({ data }) {
     height: 100%;
     border-radius: 18px;
   `;
+  const [popC, setPopC] = useState(false);
 
   return (
     <>
+      {popC && (
+        <div className={styles.pop}>
+          <div className={styles.popCategory}>
+            <span
+              onClick={() => {
+                setPopC(!popC);
+              }}
+              class="material-icons-round"
+            >
+              close
+            </span>
+          </div>
+        </div>
+      )}
+
       {width < 600 && <Nav />}
       <div className={styles.Con}>
         <div className={styles.content}>
           <div className={styles.account}></div>
           <div>
             <button className={styles.category}>{data.category}</button>
-            <span class="material-icons-outlined">mode_edit</span>
+            <span
+              onClick={() => {
+                setPopC(!popC);
+              }}
+              class="material-icons-outlined"
+            >
+              mode_edit
+            </span>
           </div>
           <div>
             <div className={styles.imgDiv}>
@@ -88,10 +112,20 @@ function LiveEdit({ data }) {
             <p>GET THE DETAILS</p>
           </div>
           <div className={styles.userDetail}>
-            <div></div>
-            <div></div>
+            <div>
+              <h2>Event Organiser</h2>
+              <p>{`${data.email}`}</p>
+            </div>
+            <div>
+              <p>{`Created on -${data.date} dsfsfsffs`} </p>
+              <p>{`Fundraising deadline ${data.deadline}`}dsfsfdf</p>
+              <p>{`Fundraising Target ${data.donationTarget}`}sdfsf</p>
+            </div>
           </div>
         </div>
+      </div>
+      <div className={styles.footer}>
+        <Footer />
       </div>
     </>
   );
