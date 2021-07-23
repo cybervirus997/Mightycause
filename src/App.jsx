54799a63c07b114c28f90e5e2f2e88eb7a8feedc
= <=>
2021-07-23 15:45:36 +0530

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import Login from './Component/Login/Login';
 import './App.css';
+import { Home } from './Component/Home/Home';
+import { Switch, Route } from 'react-router-dom';
+import Login from './Component/Login/Login';
+import SideNavbar from './Component/Dashboard/Navbar';
+import { Live } from './Component/Dashboard/Live';
 
 function App() {
   return (
     <div className='App'>
-      <Login />
+      <Switch>
+        <Route exact path='/login' component={Login}></Route>
+        <Route exact path='/' component={Home}></Route>
+        <Route exact path='/dashboard' component={SideNavbar}></Route>
+        <Route exact path='/dashboard/live' component={Live}></Route>
+      </Switch>
     </div>
   );
 }
--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,35 +1,30 @@
 import logo from './Logo.jpg';
-import Search from './Search';
 import React from 'react';
 import Footer from './Footer';
-import './Login.css';
+import styles from './Login.module.css';
 import Form from './Form';
-import {
-  AccountCircle,
-  ExploreOutlined,
-  SearchOutlined,
-} from '@material-ui/icons';
+import { ExploreOutlined, SearchOutlined } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   let iconStyles = { color: 'rgb(97, 136, 253)' };
   return (
     <div>
-      <div className='navbar'>
-        <div className='left-link'>
+      <div className={styles.navbar}>
+        <div className={styles.leftlink}>
           <button>
             <ExploreOutlined style={iconStyles} color='primary' />
           </button>
         </div>
-        <div className='logo'>
-          <img src={logo} alt='' />
-          <h3 className='name'>mightycause</h3>
-        </div>
-        <div className='right-link'>
+        <Link to='/'>
+          <div className={styles.logo}>
+            <img src={logo} alt='' />
+            <h3 className={styles.name}>mightycause</h3>
+          </div>
+        </Link>
+        <div className={styles.rightlink}>
           <button>
-            <Search style={iconStyles} color='primary'></Search>
-          </button>
-          <button>
-            <AccountCircle style={iconStyles} color='primary' />
+            <SearchOutlined style={iconStyles} color='primary' />
           </button>
         </div>
       </div>
