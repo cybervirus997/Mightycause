49c7046e384bd3d9d867c962bc669b37704fe851
cybervirus997 <soumyaranjanpurohit46@gmail.com>
2021-07-25 16:36:23 +0530

    search tab done

--- src/Component/Searchtab/Fancysearch.jsx
+++ src/Component/Searchtab/Fancysearch.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
-import AccountCircle from '@material-ui/icons/AccountCircle';
 
 const useStyles = makeStyles((theme) => ({
   margin: {
-    margin: theme.spacing(1),
+    margin: theme.spacing(0),
   },
 }));
 
@@ -17,13 +15,13 @@ export default function InputWithIcon() {
   return (
     <div>
      
-      <div className={classes.margin} style={{borderBottom:"2px solid black"}}>
-        <Grid container spacing={1} alignItems="flex-end">
+      <div className={classes.margin} >
+        <Grid container spacing={0} alignItems="flex-end">
           <Grid item>
             <span class="material-icons-outlined">search</span>
           </Grid>
           <Grid item>
-            <TextField id="input-with-icon-grid" label="Search" style={{border:"0px solid transparent"}} />
+            <TextField id="input-with-icon-grid" label="Search" style={{ paddingBottom: "6%" }}/>
           </Grid>
         </Grid>
       </div>
